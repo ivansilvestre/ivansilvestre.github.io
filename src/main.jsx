95b5e62f7d589ccd1af624fc6d137cf9af4ffdc1
Ivan Silvestre <ivanbsilvestre@gmail.com>
2025-01-05 17:30:28 +0000

    change browser router to hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { ThemeProvider } from "next-themes";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import App from "./App.jsx";
 import Gallery from "./Gallery.jsx";
 import "./index.css";
@@ -9,12 +9,12 @@ import "./index.css";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ThemeProvider attribute="class" defaultTheme="dark">
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<App />} />
           <Route path="/gallery" element={<Gallery />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </ThemeProvider>
   </StrictMode>
 );
